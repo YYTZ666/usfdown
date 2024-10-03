0e3e0b5a1c8a961d295e233418801b9e018189b6
YYTZ <132255020+YYTZ666@users.noreply.github.com>
2024-10-03 13:58:14 +0800

    Add files via upload

--- config/file.js
+++ config/file.js
@@ -1,1 +1,59 @@
-
+        // 数据config
+        const files = [
+            {
+                name: "USF0.6.30(S)",
+                description: "适配1.21.30。修复了一些bug。",
+                tags: ["正式版", "1.21"],
+                shareLink: "#",
+                feedbackLink: "#",
+                downloadLink: "/file/main/0.6.30S.zip"
+            },
+            {
+                name: "USF0.6.29(B)",
+                description: "适配1.21.0-.1.21.2。增加了功能，修复了若干bug，详细信息请查看压缩包内日志。",
+                tags: ["正式版", "1.21"],
+                shareLink: "#",
+                feedbackLink: "#",
+                downloadLink: "/file/main/0.6.29B.zip"
+            },
+            {
+                name: "USF0.6.28(B）",
+                description: "适配1.21.0-.1.21.2。修复了部分功能逻辑，修复了若干bug，详细信息请查看压缩包内日志。",
+                tags: ["正式版", "1.21"],
+                shareLink: "#",
+                feedbackLink: "#",
+                downloadLink: "/file/main/0.6.28B.zip"
+            },
+            {
+                name: "USF0.6.27(F）",
+                description: "适配1.21.0-.1.21.2。修复了若干bug，详细信息请查看压缩包内日志。",
+                tags: ["正式版", "1.21"],
+                shareLink: "#",
+                feedbackLink: "#",
+                downloadLink: "/file/main/0.6.27F.zip"
+            },
+            {
+                name: "USF0.6.26(B）",
+            description: "适配1.21.0-.1.21.2。增加了新功能，修复了若干bug，详细信息请查看压缩包内日志。",
+                tags: ["正式版", "1.21"],
+                shareLink: "#",
+                feedbackLink: "#",
+                downloadLink: "/file/main/0.6.26B.zip"
+            },
+            {
+                name: "USF0.6.25(B）",
+                description: "适配1.21.0-.1.21.2。修复了部分功能逻辑，修复了若干bug，详细信息请查看压缩包内日志。",
+                tags: ["正式版", "1.21"],
+                shareLink: "#",
+                feedbackLink: "#",
+                downloadLink: "/file/main/0.6.25B.zip"
+            },
+            {
+                name: "USF0.6.24(F）",
+                description: "适配1.21.0-.1.21.2。修复了部分功能逻辑，修复了若干bug，详细信息请查看压缩包内日志。",
+                tags: ["正式版", "1.21"],
+                shareLink: "#",
+                feedbackLink: "#",
+                downloadLink: "/file/main/0.6.24F.zip"
+            },
+        ];
