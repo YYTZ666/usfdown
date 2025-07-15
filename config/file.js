f9f33cdfd541e16f38bfe97eac344f196c2782ab
mcqcbby <1003725607@qq.com>
2025-07-15 22:01:06 +0800

    Update file.js
    Signed-off-by: mcqcbby <1003725607@qq.com>

--- config/file.js
+++ config/file.js
@@ -1,6 +1,22 @@
 // 数据config
 const files = [
         {
+         name: "USF0.7.9S",
+         description: "修复了小木斧替换功能无法使用的bug",
+         changelog: [
+                 "修复了小木斧替换功能无法使用的bug" 
+         ],
+         contributors: [
+             "USF开发团队"
+         ],
+         supportVersions: ["1.21.90","1.21.92","1.21.93"],
+         releaseDate: "2025-7-15",
+         tags: ["测试版", "1.21"],
+         downloadLink: "/files/main/USF0.7.9S(可选).zip",
+         feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
+         urgentUpdate: false  // 标记为紧急更新
+     },
+        {
          name: "USF0.7.8",
          description: "适配1.21.9x，修复了部分ui界面异常的问题，修复了小木斧无法使用的bug",
          changelog: [
@@ -8,15 +24,14 @@ const files = [
              "修复了小木斧无法使用的bug"
          ],
          contributors: [
-             "USF开发团队",
-             "EarthDLL"
+             "USF开发团队"
          ],
          supportVersions: ["1.21.90","1.21.92","1.21.93"],
          releaseDate: "2025-7-13",
          tags: ["测试版", "1.21"],
          downloadLink: "/files/main/0.7.8.zip",
          feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
-         urgentUpdate: true   // 标记为紧急更新
+         urgentUpdate: false   // 标记为紧急更新
      },
         {
          name: "USF0.7.7",
