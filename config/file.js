aa9b62285b0c3e8142b3eb90424be389a6646cc2
mcqcbby <1003725607@qq.com>
2025-07-26 02:45:41 +0800

    Update file.js
    Signed-off-by: mcqcbby <1003725607@qq.com>

--- config/file.js
+++ config/file.js
@@ -3,7 +3,7 @@
 const files = [
         {
          name: "USF0.7.11S.zip",
-         description: "修复bug更多内容请查看详情",
+         description: "修复bug兼容1.21.9X，更多内容请查看详情",
          changelog: 
         [
                  "1.修复了进入领地改冒险无法恢复的bug",
@@ -19,23 +19,7 @@ const files = [
          feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
          urgentUpdate: false  // 标记为紧急更新
      },
-        {
-         name: "USF0.7.10Fix2.zip",
-         description: "修复bug更多内容请查看详情",
-         changelog: [
-                 "修复了视角跟踪无法使用的问题",
-                 "修复因为视角跟踪报错连带游戏无法初始化，无法打开菜单的问题"
-         ],
-         contributors: [
-             "USF开发团队"
-         ],
-         supportVersions: ["1.21.90","1.21.92","1.21.93"],
-         releaseDate: "2025-7-20",
-         tags: ["测试版", "1.21"],
-         downloadLink: "/files/main/USF0.7.10Fix2.zip",
-         feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
-         urgentUpdate: true  // 标记为紧急更新
-     },
+
         {
          name: "USF0.7.10Fix2.zip",
          description: "修复bug更多内容请查看详情",
