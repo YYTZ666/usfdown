3385cba674933d93f9a75b1dc389880076850174
YYTZ666 <YYTZ666@users.noreply.github.com>
2026-06-29 17:48:51 +0800

    fix: API 路由增加 Content-Disposition inline header

--- src/pages/api/versions.js
+++ src/pages/api/versions.js
@@ -3,7 +3,8 @@ import { files } from '../../config/file.js';
 export async function GET() {
     return new Response(JSON.stringify(files), {
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json; charset=utf-8',
+            'Content-Disposition': 'inline'
         }
     });
 }
